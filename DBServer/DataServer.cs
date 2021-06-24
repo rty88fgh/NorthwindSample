803c0b69645dc1ab6821a9f6c0ed927ad66b5fce
terry <rty88fgh@gmail.com>
2021-06-24 17:28:03 +0800

    Add DB server

---
+++ DBServer/DataServer.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBServer
+{
+    public class DataServer
+    {
+        private IServiceProvider _provider;
+        public DataServer(IServiceProvider provider)
+        {
+            _provider = provider;
+        }
+    }
+}
