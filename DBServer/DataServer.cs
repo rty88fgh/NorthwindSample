8edf77d0e18ea9e3781171a50575c36a9cd923f0
terry <rty88fgh@gmail.com>
2021-06-25 10:00:46 +0800

    DB and controller

--- DBServer/DataServer.cs
+++ DBServer/DataServer.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using AutoMapper;
+using DBServer.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,9 +15,97 @@ namespace DBServer
     public class DataServer
     {
         private IServiceProvider _provider;
-        public DataServer(IServiceProvider provider)
+        private ILogger<DataServer> _logger;
+        private IMapper _mapper;
+        public DataServer(IServiceProvider provider, ILogger<DataServer> logger, IMapper mapper)
         {
             _provider = provider;
+            _logger = logger;
+            _mapper = mapper;
         }
+
+        public async Task<IEnumerable<Employee>> GetAllEmployee()
+        {
+            using var db = _provider.CreateScope().ServiceProvider.GetService<DataServerContext>();
+            try
+            {
+                var allEmployees = await db.Employees.ToListAsync();
+                return allEmployees;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error while getting employee");
+                return null;
+            }
+        }
+
+        public async Task<bool> InsertOrUpdateEmployee(Employee editingEmployee)
+        {
+            if (editingEmployee == null)
+            {
+                _logger.LogError("Failed to get employee to insert/update ");
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(editingEmployee.FirstName) || string.IsNullOrEmpty(editingEmployee.LastName))
+            {
+                _logger.LogError("Please enter first name and last name");
+                return false;
+            }
+
+            using var db = _provider.CreateScope().ServiceProvider.GetService<DataServerContext>();
+            try
+            {
+                var origin = await db.Employees.FirstOrDefaultAsync(employee => employee.EmployeeId == editingEmployee.EmployeeId);
+                if (origin == null)
+                {
+                    origin = editingEmployee;
+                    await db.Employees.AddAsync(editingEmployee);
+                }
+                else
+                    _mapper.Map(editingEmployee, origin);
+
+                await db.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error while interting or updating employee");
+                return false;
+            }
+        }
+
+        public async Task<bool> DeleteEmplyee(int employeeId)
+        {
+
+            using var db = _provider.CreateScope().ServiceProvider.GetService<DataServerContext>();
+            try
+            {
+                var origin = await db.Employees.Include(employee => employee.Orders)
+                                               .Include(employee => employee.EmployeeTerritories)
+                                               .Include(employee => employee.InverseReportsToNavigation)
+                                               .FirstOrDefaultAsync(employee => employee.EmployeeId == employeeId);
+                if (origin == null)
+                {
+                    _logger.LogError("Failed to find removing employee");
+                    return false;
+                }
+
+                db.EmployeeTerritories.RemoveRange(origin.EmployeeTerritories);
+                origin.InverseReportsToNavigation.ToList().ForEach(employee => employee.ReportsTo = null);
+                db.Employees.Remove(origin);
+                origin.Orders.ToList().ForEach(order => order.Employee = null);
+
+                await db.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error while deleting employee");
+                return false;
+            }
+        }
+
+
     }
 }
---
+++ NorthwindSample/Controller/EmployeesController.cs
@@ -0,0 +1,52 @@
+﻿using DBServer;
+using DBServer.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NorthwindSample.Controller
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private DataServer _dataServer;
+        public EmployeesController(DataServer dataServer)
+        {
+            _dataServer = dataServer;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var allEmployees = await _dataServer.GetAllEmployee();
+
+            //present cycle when serialize
+            allEmployees.ToList().ForEach(employee => employee.InverseReportsToNavigation = null);
+
+            if (allEmployees != null)
+                return Ok(allEmployees);
+            else
+                return NotFound();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Add(Employee employee)
+        {
+            return Ok(await _dataServer.InsertOrUpdateEmployee(employee));
+        }
+        [HttpPost]
+        public async Task<IActionResult> Update(Employee employee)
+        {
+            return Ok(await _dataServer.InsertOrUpdateEmployee(employee));
+        }
+        [HttpDelete("{employeeID}")]
+        public async Task<IActionResult> Delete(int employeeID)
+        {
+            return Ok(await _dataServer.DeleteEmplyee(employeeID));
+        }
+
+
+    }
+}
